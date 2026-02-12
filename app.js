6715d946bc231b774c51b3bcc66020ac527c07b2
yoheshwar <yogeshroa23@gmail.com>
2026-02-12 16:44:52 +0000

    fix code app.js

--- app.js
+++ app.js
@@ -11,14 +11,25 @@ const dotenv = require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://market-mate-project.netlify.app/",
+];
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  })
+  }),
 );
 app.use(cookieParser());
 app.use(morgan("dev"));
